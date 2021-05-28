6ad8aebdc5a2393cd2dd3e87e66f9084e1c942cd
Oleg Lihogub <oligli@yandex.ru>
2021-05-28 22:21:17 +0300

    refactor

--- backend/src/main/java/ru/obschaga/ObschagaApplication.java
+++ backend/src/main/java/ru/obschaga/ObschagaApplication.java
@@ -1,13 +1,13 @@
-package ru.lihogub.babystep;
+package ru.obschaga;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BabyStepApplication {
+public class ObschagaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BabyStepApplication.class, args);
+		SpringApplication.run(ObschagaApplication.class, args);
 	}
 
 }
