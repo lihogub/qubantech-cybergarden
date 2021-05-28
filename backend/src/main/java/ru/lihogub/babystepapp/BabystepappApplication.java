8209557e0b8886fa2db59b94b3277a807a6c66fa
Oleg Lihogub <oligli@yandex.ru>
2021-05-28 15:21:52 +0300

    spring added

---
+++ backend/src/main/java/ru/lihogub/babystepapp/BabystepappApplication.java
@@ -0,0 +1,13 @@
+package ru.lihogub.babystepapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabystepappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabystepappApplication.class, args);
+	}
+
+}
