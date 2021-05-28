c08d2f6355c435346238ba719a3ec8375f80f574
v.shustov <57774085+shuimi@users.noreply.github.com>
2021-05-28 19:16:15 +0300

    Calendar

--- frontend/src/pages/Test.js
+++ frontend/src/pages/Test.js
@@ -1,8 +1,8 @@
 import Header from '../components/Header'
 import Meta from '../components/Meta'
-import {format} from "date-fns";
 import * as React from "react";
-// import {DefaultMonthlyEventItem, MonthlyBody, MonthlyCalendar, MonthlyNav} from "@zach.codes/react-calendar";
+import {MonthlyBody, MonthlyCalendar, MonthlyNav} from "@zach.codes/react-calendar";
+import '@zach.codes/react-calendar/dist/calendar-tailwind.css';
 
 const Test = () => {
   // page content
@@ -14,10 +14,29 @@ const Test = () => {
       <Meta title={pageTitle}/>
       <Header head={pageTitle} description={pageDescription} />
 
-        {/*<MonthlyCalendar currentMonth={1} onCurrentMonthChange={()=>{}}>*/}
-        {/*    <MonthlyNav />*/}
-        {/*    <MonthlyBody  children={'a'} events={()=>{}}/>*/}
-        {/*</MonthlyCalendar>*/}
+        <MonthlyCalendar
+            currentMonth={new Date("2021-04-30T21:00:00.000Z")}
+            onCurrentMonthChange={function noRefCheck(){}}
+        >
+            <MonthlyNav />
+            <MonthlyBody
+                events={[
+                    {
+                        date: new Date('2021-05-28T14:08:20.868Z'),
+                        title: 'Call John'
+                    },
+                    {
+                        date: new Date('2021-05-28T15:08:20.868Z'),
+                        title: 'Call John'
+                    },
+                    {
+                        date: new Date('2021-05-28T16:08:20.868Z'),
+                        title: 'Meeting with Bob'
+                    }
+                ]}
+                renderDay={function noRefCheck(){}}
+            />
+        </MonthlyCalendar>
 
     </div>
   )
