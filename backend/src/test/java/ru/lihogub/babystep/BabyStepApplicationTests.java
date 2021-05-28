75cea5b290092e5bdf1807381410d775b34cf604
Oleg Lihogub <oligli@yandex.ru>
2021-05-28 16:38:06 +0300

    init spring basic controller

--- backend/src/test/java/ru/lihogub/babystep/BabyStepApplicationTests.java
+++ backend/src/test/java/ru/lihogub/babystep/BabyStepApplicationTests.java
@@ -1,10 +1,10 @@
-package ru.lihogub.babystepapp;
+package ru.lihogub.babystep;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BabystepappApplicationTests {
+class BabyStepApplicationTests {
 
 	@Test
 	void contextLoads() {
