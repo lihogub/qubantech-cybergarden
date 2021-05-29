422e9035b4da7ccd583cec71765219c8ab2a72cf
v.shustov <57774085+shuimi@users.noreply.github.com>
2021-05-29 05:49:12 +0300

    axios

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import Header from "../components/Header";
 
+const axios = require('axios');
+let temp;
+
+// Make a request for a user with a given ID
+axios.get({
+    method: 'get',
+    url: 'http://192.168.1.152:8080/user/2'
+})
+    .then((response) => {
+        // handle success
+        temp = response;
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+    .then(() => {});
+
 export default ()=>{
     return(<div>
-
+        {temp}
     </div>);
 }
