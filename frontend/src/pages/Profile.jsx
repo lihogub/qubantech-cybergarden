f917d30c13c828f33de2339d210510e2d0763091
v.shustov <57774085+shuimi@users.noreply.github.com>
2021-05-29 13:42:49 +0300

    profile

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from "../components/Header";
+import axios from "./../backendInterface";
 
-const axios = require('axios');
-let temp;
+let user_id = 1;
 
-// Make a request for a user with a given ID
-axios.get({
-    method: 'get',
-    url: 'http://192.168.1.152:8080/user/2'
-})
-    .then((response) => {
-        // handle success
-        temp = response;
-    })
-    .catch((error) => {
-        console.log(error);
-    })
-    .then(() => {});
+class Profile extends React.Component {
 
-export default ()=>{
-    return(<div>
-        {temp}
-    </div>);
+    componentDidMount() {
+
+        axios.get('http://192.168.1.152:8080/user/' + user_id)
+            .then((response) => {
+                const response_data = response['data'];
+
+                this.setState({profile: response_data});
+
+                console.log(response_data);
+                console.log(this.state);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+            .then(() => {});
+
+    }
+
+    render() {
+        return(
+            <div>
+                {this.state && this.state.profile['firstname']}
+            </div>
+        );
+    }
 }
+
+export default Profile;
