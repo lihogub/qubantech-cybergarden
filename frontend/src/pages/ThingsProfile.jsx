b9bd3ae1770ee8a1a99226eb402429b7e84d4de1
Nikita Vanychenko <postamentbox@bk.ru>
2021-05-30 14:07:04 +0300

    thiiings

--- frontend/src/pages/ThingsProfile.jsx
+++ frontend/src/pages/ThingsProfile.jsx
@@ -39,7 +39,109 @@ export default function ThingsProfile() {
                     <div className="container mx-auto px-4">
                         <div className="relative flex flex-col w-full items-center justify-center">
                             <ul className="w-full">
-                                <li className="border-gray-500 flex flex-col mb-10 gap-y-3 ">
+                                <li className="border-gray-500 flex flex-col mb-10 gap-y-1 ">
+                                    <h1 className="text-3xl sm:text-4xl font-semibold leading-normal text-gray-900 text-left mt-4">
+                                        Отклики на твои предложения
+                                    </h1>
+                                    <div
+                                        className="overflow-hidden shadow-lg rounded-lg h-90 w-full cursor-pointer m-auto mb-0">
+                                        <p className="text-black dark:text-gray-300 font-light text-md">
+                                            По "Кошачий корм" (+10)
+                                        </p>
+                                    </div>
+                                    <div
+                                        className="overflow-hidden shadow-lg rounded-lg h-90 w-full cursor-pointer m-auto">
+                                        <a href="#" className="w-full block h-full">
+                                            <div className="bg-white dark:bg-gray-800 w-full p-3">
+                                                <p className="text-gray-800 dark:text-white text-xl font-medium mb-1">
+                                                    Привет, я очень голоден!!
+                                                </p>
+                                                <p className="text-gray-400 dark:text-gray-300 font-light text-md">
+                                                    Корпус Б, этаж 2, комната 231
+                                                </p>
+                                                <p className="text-gray-400 dark:text-gray-300 font-light text-md">
+                                                    Обменов за последний месяц - 5
+                                                </p>
+                                                <div className="flex items-center mt-4">
+                                                    <a href="#" className="block relative min-w-10 max-w-10">
+                                                        <img alt="profil" src="https://i.redd.it/v0caqchbtn741.jpg"
+                                                             className="mx-auto object-cover rounded-full h-10 max-w-sm"/>
+                                                    </a>
+                                                    <div className="flex flex-col justify-between ml-4 text-sm">
+                                                        <p className="text-gray-800 w-max dark:text-white">
+                                                            Петр Иванов
+                                                        </p>
+                                                        <p className="text-gray-400 w-max dark:text-gray-300">
+                                                            6 часов назад
+                                                        </p>
+                                                    </div>
+                                                    <div className={'w-full'}></div>
+                                                    <div className={"items-end text-3xl"}>
+                                                        <div>
+                                                            +2
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="bg-white rounded-lg w-full mt-2 m-auto">
+                                                    <div className="w-full h-4 bg-gray-400 rounded-full">
+                                                        <div
+                                                            className="w-1/5 h-full text-center text-xs text-black bg-purple rounded-full">
+                                                            20
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div
+                                        className="overflow-hidden shadow-lg rounded-lg h-90 w-full cursor-pointer m-auto">
+                                        <a href="#" className="w-full block h-full">
+                                            <div className="bg-white dark:bg-gray-800 w-full p-3">
+                                                <p className="text-gray-800 dark:text-white text-xl font-medium mb-1">
+                                                    Моя кошка была бы рада!
+                                                </p>
+                                                <p className="text-gray-400 dark:text-gray-300 font-light text-md">
+                                                    Корпус Б, этаж 2, комната 236
+                                                </p>
+                                                <p className="text-gray-400 dark:text-gray-300 font-light text-md">
+                                                    Обменов за последний месяц - 2
+                                                </p>
+                                                <div className="flex items-center mt-4">
+                                                    <a href="#" className="block relative min-w-10 max-w-10">
+                                                        <img alt="profil" src="https://i.redd.it/v0caqchbtn741.jpg"
+                                                             className="mx-auto object-cover rounded-full h-10 max-w-sm"/>
+                                                    </a>
+                                                    <div className="flex flex-col justify-between ml-4 text-sm">
+                                                        <p className="text-gray-800 w-max dark:text-white">
+                                                            Иванов Петр
+                                                        </p>
+                                                        <p className="text-gray-400 w-max dark:text-gray-300">
+                                                            3 часа назад
+                                                        </p>
+                                                    </div>
+                                                    <div className={'w-full'}></div>
+                                                    <div className={"items-end text-3xl"}>
+                                                        <div>
+                                                            +1
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="bg-white rounded-lg w-full mt-2 m-auto">
+                                                    <div className="w-full h-4 bg-gray-400 rounded-full">
+                                                        <div
+                                                            className="w-1/5 h-full text-center text-xs text-black bg-purple rounded-full">
+                                                            20
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </div>
+
+
+
+
+
                                     <h1 className="text-3xl sm:text-4xl font-semibold leading-normal text-gray-900 text-left mt-4">
                                         Твои предложения
                                     </h1>
@@ -69,7 +171,7 @@ export default function ThingsProfile() {
                                                     <div className={'w-full'}></div>
                                                     <div className={"items-end text-3xl"}>
                                                         <div>
-                                                            -3
+                                                            -2 <div className={"text-sm w-max"}>(Требуют -3) </div>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -81,55 +183,6 @@ export default function ThingsProfile() {
                                                         <div
                                                             className="w-2/5 h-full text-center text-xs text-black bg-purple rounded-full">
                                                             40
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </div>
-                                    <h1 className="text-3xl sm:text-4xl font-semibold leading-normal text-gray-900 text-left mt-4">
-                                        Успешные сделки
-                                    </h1>
-                                    <div
-                                        className="overflow-hidden shadow-lg rounded-lg h-90 w-full cursor-pointer m-auto">
-                                        <a href="#" className="w-full block h-full">
-                                            <img alt="blog photo" src="https://avatars.mds.yandex.net/get-market-ugc/223176/2a000001795f991d0f96e7ca51fa8711f2ee/1920-1920"
-                                                 className="max-h-40 w-full object-cover"/>
-                                            <div className="bg-white dark:bg-gray-800 w-full p-3">
-                                                <p className="text-gray-800 dark:text-white text-xl font-medium mb-1">
-                                                    Кошачий корм
-                                                </p>
-                                                <p className="text-gray-400 dark:text-gray-300 font-light text-md">
-                                                    Дорогой моему сердцу корм. Но кошки у меня нет, поэтому отдам кому-нибудь за хорошую карму :)
-                                                </p>
-                                                <div className="flex items-center mt-4">
-                                                    <a href="#" className="block relative min-w-10 max-w-10">
-                                                        <img alt="profil" src="https://i.redd.it/v0caqchbtn741.jpg"
-                                                             className="mx-auto object-cover rounded-full h-10 max-w-sm"/>
-                                                    </a>
-                                                    <div className="flex flex-col justify-between ml-4 text-sm">
-                                                        <p className="text-gray-800 w-max dark:text-white">
-                                                            Петр Иванов
-                                                        </p>
-                                                        <p className="text-gray-400 w-max dark:text-gray-300">
-                                                            6 часов назад
-                                                        </p>
-                                                    </div>
-                                                    <div className={'w-full'}></div>
-                                                    <div className={"items-end text-3xl"}>
-                                                        <div>
-                                                            -10
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div className={"text-gray-800 dark:text-white text-sm mt-2"}>
-                                                    Корпус Б, этаж 2, комната 231
-                                                </div>
-                                                <div className="bg-white rounded-lg w-full mt-2 m-auto">
-                                                    <div className="w-full h-4 bg-gray-400 rounded-full">
-                                                        <div
-                                                            className="w-2/5 h-full text-center text-xs text-black bg-purple rounded-full">
-                                                            25
                                                         </div>
                                                     </div>
                                                 </div>
