6047b52184116fc5733b79db58e7b54a290a2efb
uliatitova <tulia_87@mail.ru>
2021-05-30 11:18:52 +0300

    dialogs doesnt work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Profile from './pages/Profile'
 import Dialogs from './pages/Dialogs'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
+import Chat from './pages/Chat'
 
 function App() {
   return (
@@ -12,10 +13,12 @@ function App() {
           <Link to="/">Home</Link>
           <Link to="/profile">Profile</Link>
           <Link to="/dialogs">Dialogs</Link>
+          <Link to="/chat">Chat</Link>
           <Switch>
               <Route path='/' component={About} exact />
               <Route path='/profile' component={Profile} />
               <Route path='/dialogs' component={Dialogs} />
+              <Route path='/chat' component={Chat} />
               <Route component={NotFound} />
           </Switch>
       </Router>
